26532d180f70f261b85a202b74b902aff3940584
adria <littleivanitsky@gmail.com>
2022-03-22 12:38:27 -0700

    author insert test written

---
+++ __tests__/author.test.js
@@ -0,0 +1,24 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('local-bookstore routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+
+  test('should create an author table row', async () => {
+    const expected = {
+      name: 'Haruki Murakami',
+      dob: '1949-01-12',
+      pob: 'Kyoto, Japan',
+    };
+    const resp = await request(app).post('/api/v1/authors').send(expected);
+    expect(resp.body).toEqual({ id: expect.any(String), ...expected });
+  });
+});
