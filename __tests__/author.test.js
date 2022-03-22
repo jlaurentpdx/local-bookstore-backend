c0f5a5bd7f2d598925edd56c79cfbc8a20faed33
mira-kine <mirak0419@gmail.com>
2022-03-22 14:34:41 -0700

    authors get all test passing with controller

--- __tests__/author.test.js
+++ __tests__/author.test.js
@@ -21,4 +21,29 @@ describe('local-bookstore routes', () => {
     const resp = await request(app).post('/api/v1/authors').send(expected);
     expect(resp.body).toEqual({ author_id: expect.any(String), ...expected });
   });
+
+  test('should display all authors', async () => {
+    const expected = [
+      {
+        author_id: '1',
+        name: 'Tom Robbins',
+        dob: '1932-07-22T08:00:00.000Z',
+        pob: 'Blowing Rock, NC, US',
+      },
+      {
+        author_id: '2',
+        name: 'Liu Cixin',
+        dob: '1963-06-23T07:00:00.000Z',
+        pob: 'Beijing, China',
+      },
+      {
+        author_id: '3',
+        name: 'Oliver Sacks',
+        dob: '1933-07-09T08:00:00.000Z',
+        pob: 'London, England',
+      },
+    ];
+    const resp = await request(app).get('/api/v1/authors');
+    expect(resp.body).toEqual(expect.arrayContaining(expected));
+  });
 });
